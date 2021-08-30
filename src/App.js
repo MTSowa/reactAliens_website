9f95b8366e4a301f9d47fc64d5639173f23a7791
MTSowa <sowaf57@gmail.com>
2021-08-30 14:18:35 +0000

    IC - setup and fetch call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
+import React, { Component } from 'react';
+import Cardlist from './components/Card-list/Cardlist.compo';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      aliens: [],
+    };
+  }
+
+  // mount
+  componentDidMount() {
+    try {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((alienData) => this.setState({ aliens: alienData }));
+      // if(!response.ok) throw new Error('there was a problem fetching the data..')
+    } catch (error) {
+      console.log('there has been an error...');
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Cardlist>
+          {this.state.aliens.map((alien) => (
+            <div key={alien.id}>
+              <h3>{alien.name}</h3>
+            </div>
+          ))}
+        </Cardlist>
+      </div>
+    );
+  }
 }
-
-export default App;
